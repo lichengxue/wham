6316b6e84bfb4f76b1e2524ece479f0743295ac4
Li, Chengxue <43245366+lichengxue@users.noreply.github.com>
2024-04-24 15:35:11 -0400

    Update advice_fn.R

--- R/advice_fn.R
+++ R/advice_fn.R
@@ -1,3 +1,18 @@
+#' Specify configuration for fully-selected fishing mortality
+#'
+#' @param input list containing data, parameters, map, and random elements (output from \code{\link{wham::prepare_wham_input}})
+#' @param F_opts (optional) named list of initial values for annual fully-selected fishing mortality and configuration method for estimation.
+#' 
+#' \code{F_opts} specifies a few as well as the effect on the population. Environmental covariate data need not span
+#' the same years as the fisheries data. It can be \code{NULL} if no environmental data are to be fit.
+#' Otherwise, it must be a named list with the following components:
+#'   \describe{
+#'     \item{$F}{matrix (n_years x n_fleets) of (initial) values for fully-selected fishing morality.}
+#'     \item{$F_config}{integer 1: (default) configure F parameters (on log scale) as an F in the initial year and then deviations from one year to the next,
+#'        or 2: configure F parameters as (log) annual values.}
+#'  }
+#'
+#' @export
 # Function to calculate catch advice
 advice_fn = function(em, pro.yr = assess.interval){
   #make 5 year projections using F40. Use average SSB/R and YPR inputs over most recent 5 years
