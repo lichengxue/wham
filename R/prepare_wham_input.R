db25409778d03fef713ab914691bf28215f37e1b
Li, Chengxue <43245366+lichengxue@users.noreply.github.com>
2026-03-04 22:55:46 -0500

    fix bug

--- R/prepare_wham_input.R
+++ R/prepare_wham_input.R
@@ -455,9 +455,10 @@ set_basic_info <- function(input, basic_info){
   
   # --------------------------------------------------------------- #
   
-  n_stocks  <- as.integer(basic_info['n_stocks'])
-  n_regions <- as.integer(basic_info['n_regions'])
-  n_seasons <- as.integer(basic_info['n_seasons'])
+  n_stocks  <- as.integer(input$data$n_stocks)
+  n_regions <- as.integer(input$data$n_regions)
+  n_seasons <- as.integer(input$data$n_seasons)
+  n_ages    <- as.integer(input$data$n_ages)
   
   # 1. Movement dynamics flag
   if (!is.null(basic_info$move_dyn)) {
